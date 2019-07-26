595a7950ee37705dccc437b9c8c612b51d2d3268
TARUN <tarunyadav83333@gmail.com>
2019-07-26 16:01:16 +0530

    added normal gsm i/p

--- GSM http request/main.cpp
+++ GSM http request/main.cpp
@@ -6,13 +6,38 @@
  */ 
 
 #include <avr/io.h>
+#include "USART.h"
+#include <util/delay.h>
 
+void Send_String(char ar[]){
+	int i;
+	i=0;
+	while(ar[i]!='\0'){
+		USART_Transmit(ar[i]);
+		i++;
+	}
+}
 
 int main(void)
 {
+	
+	
+	
     /* Replace with your application code */
     while (1) 
     {
+		Send_String("AT+HTTPINIT");		//init http service
+		_delay_ms(1000);
+		
+		Send_String("AT+HTTPPARA=”CID”,1");			//set http session
+		_delay_ms(1000);
+		
+		Send_String("AT+HTTPPARA=\”URL\”,\”google.com\”");			//set http url
+		_delay_ms(1000);
+		
+		Send_String("AT+HTTPACTION=0");			//return 200 if successful
+		_delay_ms(1000);
+		
     }
 }
 
